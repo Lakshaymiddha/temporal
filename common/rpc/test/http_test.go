a1e146268675690305be5ee51c276dd9d7e1f63a
Stephan Behnke <stephanos@users.noreply.github.com>
2026-08-13 10:01:22 -0700

    Fix resolver registration lifecycle

--- common/rpc/test/http_test.go
+++ common/rpc/test/http_test.go
@@ -30,6 +30,8 @@ func TestCreateLocalFrontendHTTPClient_UsingMembership(t *testing.T) {
 	require.NoError(t, err)
 	monitor.EXPECT().GetResolver(primitives.FrontendService).Return(resolver, nil)
 	resolver.EXPECT().AvailableMembers().Return([]membership.HostInfo{membership.NewHostInfoFromAddress(addr.String())})
+	frontendMembershipAddress, cleanup := membership.GRPCResolverURLForTesting(monitor, primitives.FrontendService)
+	t.Cleanup(cleanup)
 
 	fact := rpc.NewFactory(
 		nil,
@@ -37,8 +39,8 @@ func TestCreateLocalFrontendHTTPClient_UsingMembership(t *testing.T) {
 		nil, // No logger
 		nil, // No metrics handler
 		nil,
-		membership.GRPCResolverURLForTesting(monitor, primitives.FrontendService),
-		membership.GRPCResolverURLForTesting(monitor, primitives.FrontendService),
+		frontendMembershipAddress,
+		frontendMembershipAddress,
 		int(port),
 		nil, // No TLS
 		nil,
@@ -63,6 +65,8 @@ func TestCreateLocalFrontendHTTPClient_UsingFixedHostPort(t *testing.T) {
 	}))
 	defer srv.Close()
 	addr := srv.Listener.Addr()
+	frontendMembershipAddress, cleanup := membership.GRPCResolverURLForTesting(nil, primitives.FrontendService)
+	t.Cleanup(cleanup)
 
 	fact := rpc.NewFactory(
 		nil, // unused
@@ -70,7 +74,7 @@ func TestCreateLocalFrontendHTTPClient_UsingFixedHostPort(t *testing.T) {
 		nil, // No logger
 		nil, // No metrics handler
 		nil,
-		membership.GRPCResolverURLForTesting(nil, primitives.FrontendService),
+		frontendMembershipAddress,
 		addr.String(),
 		0,   // Port is unused
 		nil, // No TLS
@@ -97,6 +101,8 @@ func TestCreateLocalFrontendHTTPClient_UsingFixedHostPort_AndTLS(t *testing.T) {
 	defer srv.Close()
 	addr := srv.Listener.Addr()
 	tlsConfig := srv.Client().Transport.(*http.Transport).TLSClientConfig
+	frontendMembershipAddress, cleanup := membership.GRPCResolverURLForTesting(nil, primitives.FrontendService)
+	t.Cleanup(cleanup)
 
 	fact := rpc.NewFactory(
 		nil, // unused
@@ -104,7 +110,7 @@ func TestCreateLocalFrontendHTTPClient_UsingFixedHostPort_AndTLS(t *testing.T) {
 		nil, // No logger
 		nil, // No metrics handler
 		nil,
-		membership.GRPCResolverURLForTesting(nil, primitives.FrontendService),
+		frontendMembershipAddress,
 		addr.String(),
 		0, // Port is unused
 		tlsConfig,
